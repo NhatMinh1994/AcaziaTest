ada3bd3e2297d768de3e7d961a5c4f7ef37ce77d
@minhmyman1994 <minhmyman1994@gmail.com>
2020-10-07 22:19:51 +0700

    update

--- App.js
+++ App.js
@@ -1,114 +1,164 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { Component } from 'react';
+import { View, Text, FlatList, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { chainParse } from './src/utils';
+import imgGender from './src/images/imgGender.png'
+import imgCalendar from './src/images/imgCalendar.png'
+import imgLock from './src/images/imgLock.png'
+import imgMap from './src/images/imgMap.png'
+import imgPhone from './src/images/imgPhone.png'
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+const dataChoose = [
+  {
+    id: 1,
+    title: "Gender",
+    source: imgGender,
+    key:""
+  },
+  {
+    id: 2,
+    title: "Date of birth",
+    source: imgCalendar
+  },
+  {
+    id: 3,
+    title: "Address",
+    source: imgMap
+  },
+  {
+    id: 4,
+    title: "Phone number",
+    source: imgPhone
+  },
+  {
+    id: 5,
+    title: "Lock",
+    source: imgLock
+  }
+]
+export default class App extends Component {
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: "",
+      indexFocus: 2
+    };
+  }
+  getImagesFromApi = () => {
+    return fetch('https://randomuser.me/api/0.4/?randomapi')
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json)
+        const data = chainParse(json, ["results", 0]) || ""
+        this.setState({
+          data
+        })
+        return json;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  componentDidMount() {
+    this.getImagesFromApi()
+  }
+  _handleOnPressItem = (indexFocus) => {
+    this.setState({
+      indexFocus
+    })
+  }
+  _renderItem = (item, index) => {
+    const source = chainParse(item, ["source"]) || ""
+    const { indexFocus } = this.state
+    return (
+      <TouchableOpacity onPress={()=>this._handleOnPressItem(index)}>
+        <View style={styles.viewItem}>
+          <View style={[styles.borderTopItem, { backgroundColor: index == indexFocus ? "green" : "gray" }]}></View>
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+          <Image source={source} style={[styles.imgItem, { tintColor: index == indexFocus ? "green" : "gray" }]}></Image>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+  _renderInfo=()=>{
+    const {indexFocus,data}= this.state
+    const title = dataChoose[indexFocus].title
+    return(
+      <View>
+        <Text style={styles.txtTitle}>hihi</Text>
+        <Text style={styles.txtDetail}>hehe</Text>
+        
+      </View>
+    )
+  }
+  render() {
+    const { data } = this.state
+    const picture = chainParse(data, ["user", "picture"]) || ""
+    console.log(picture)
+    return (
+      <View style={{ flex: 1, backgroundColor: 'blue' }}>
+        <View style={styles.container}>
+          <View style={styles.viewHeader}></View>
+          {!!picture && <Image source={{ uri: picture }} style={styles.imgProfile}></Image>}
+          {!picture && <View style={styles.imgProfile}></View>}
+      <View style={{width:'100%', height:32}}></View>
+      {/* {this._renderInfo()} */}
+      <View style={{width:'100%', height:32}}></View>
+      
+          <View style={{ flexDirection: 'row', marginTop: 16 }}>
+            {dataChoose.map(this._renderItem)}
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
-
+        </View>
+      </View>
+    );
+  }
+}
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  borderTopItem: {
+    width: 20,
+    height: 3
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  container: {
+    flex: 1,
+    marginHorizontal: 16,
+    marginTop: 12,
+    backgroundColor: 'white',
+    alignItems: 'center'
   },
-  body: {
-    backgroundColor: Colors.white,
+  imgItem: {
+    width: 20,
+    height: 20,
+    resizeMode: 'contain',
+    tintColor: "gray",
+    marginTop: 8
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+ 
+  viewHeader: {
+    height: 120,
+    width: '100%',
+    backgroundColor: 'gray',
+    opacity: 0.3
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  imgProfile: {
+    marginTop: -80,
+    height: 160,
+    width: 160,
+    borderRadius: 80,
+    backgroundColor: 'gray'
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+  viewItem: {
+    marginLeft: 20
   },
-  highlight: {
-    fontWeight: '700',
+  txtTitle:{
+    fontSize:16,
+    color:"gray",
+    fontWeight:400
   },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+  txtDetail:{
+    fontSize:20,
+    color:"black",
+    fontWeight:"bold",
+    marginTop:10
+  }
 
-export default App;
+})
